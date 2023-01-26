7939e712ab7408d9fe0751b1ce0effee5ade922a
metal_land <kirill.bronovicki@gmail.com>
2023-01-26 21:08:55 +0100

    Learning to change a theme

--- app/src/main/java/com/example/googlecodelabs/MainActivity.kt
+++ app/src/main/java/com/example/googlecodelabs/MainActivity.kt
@@ -15,23 +15,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.calculateButton.setOnClickListener { calculateTip() }
-    }
-
-    private fun calculateTip() {
-        val costOfService = binding.costOfService.text.toString().toDouble()
-        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-            R.id.option_twenty_percent -> 20
-            R.id.option_eighteen_percent -> 18
-            else -> 15
-        }
-        var finalSum = costOfService * tipPercentage / 100
-        if (binding.roundUpTip.isChecked) {
-            finalSum = ceil(finalSum)
-        }
-        binding.tipAmount.text = getString(
-            R.string.tip_amount_result,
-            NumberFormat.getCurrencyInstance().format(finalSum)
-        )
     }
 }
