4dd942b3897c245b158fe4859fb96c6839882ffa
metal_land <kirill.bronovicki@gmail.com>
2023-01-29 21:16:31 +0100

    Tests

---
+++ app/src/androidTest/java/com/example/googlecodelabs/MainActivityTest.kt
@@ -0,0 +1,42 @@
+package com.example.googlecodelabs
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.containsString
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Tests main activity.
+ */
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun calculate_20_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+            .perform(ViewActions.closeSoftKeyboard())
+
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+
+        onView(withId(R.id.tip_amount))
+            .check(matches(withText(containsString("$10.00"))))
+    }
+
+}
